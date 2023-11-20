97d1532f30f3f72c5588db7f1f6133cf22f39592
Sahil Pulikal <sahilpulikal@gmail.com>
2023-11-20 23:31:09 +0530

    course creation backend done

--- src/app/api/course/createChapters/route.ts
+++ src/app/api/course/createChapters/route.ts
@@ -4,6 +4,8 @@ import { NextResponse } from "next/server";
 import { createChaptersSchema } from "@/validators/course";
 import { ZodError } from "zod";
 import { strict_output } from "@/lib/gpt";
+import { getUnsplashImage } from "@/lib/unsplash";
+import { prisma } from "@/lib/db";
 
 export async function POST(req: Request, res: Response) {
     try {
@@ -16,7 +18,7 @@ export async function POST(req: Request, res: Response) {
                 youtube_search_query: string;
                 chapter_title: string;
             }[];
-        };
+        }[];
 
         //prompt engineering
         let output_units: outputUnits = await strict_output(
@@ -28,8 +30,48 @@ export async function POST(req: Request, res: Response) {
             }
         );
 
-        console.log(output_units);
-        return NextResponse.json(output_units);
+        const imageSearchTerm = await strict_output(
+            'you are an AI capapble of finding the most relevant image for a course',
+            `Please provide a good image search term for the title of a course about ${title}. This search term will be fed into the unsplash API, so make sure it is a good search term that will return good results`,
+            {
+                image_search_term: 'a good search term for the title of the course'
+            }
+        );
+
+        const course_image = await getUnsplashImage(
+            imageSearchTerm.image_search_term
+        );
+
+        const course = await prisma.course.create({
+            data: {
+                name: title,
+                image: course_image,
+            },
+        });
+
+        for (const unit of output_units) {
+            const title = unit.title;
+            const prismaUnit = await prisma.unit.create({
+                data: {
+                    name: title,
+                    courseId: course.id
+                }
+            })
+            await prisma.chapter.createMany({
+                data: unit.chapters.map((chapter)=>{
+                    return {
+                        name: chapter.chapter_title,
+                        youtubeSearchQuery: chapter.youtube_search_query,
+                        unitId: prismaUnit.id
+                    }
+                })
+            })
+        }
+
+        // console.log(output_units);
+        // return NextResponse.json({output_units, imageSearchTerm, course_image});
+        return NextResponse.json({ course_id: course.id });
+
     } catch (error) {
         if (error instanceof ZodError) {
             return new NextResponse("invalid body", {status: 400});
--- src/lib/unsplash.ts
+++ src/lib/unsplash.ts
@@ -4,9 +4,8 @@ import axios from 'axios'
 // }
 
 export const getUnsplashImage = async (query: string) => {
-    const imageResponseRaw = await fetch(
+    const { data } = await axios.get(
       `https://api.unsplash.com/search/photos?per_page=1&query=${query}&client_id=${process.env.UNSPLASH_API_KEY}`
     );
-    const imageResponse = await imageResponseRaw.json();
-    return imageResponse.results[0].urls.small_s3;
+    return data.results[0].urls.small_s3;
   };
